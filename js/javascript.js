b86965aef43a479fd2c37629358d3222a07e5bdf
cksumalinog <clarencekenzosumalinog@gmail.com>
2025-10-28 22:12:58 +0800

    learned java variables

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,14 @@
-console.log("Welcome!");
+let variable1 = "Kenzo";
+let variable2 = 2;
+let variable3 = 6;
+console.log(variable1);
+
+console.log(variable1 + (variable2 + variable3));
+
+let variables = variable1 + (variable2 + variable3);
+
+console.log(variables);
+
+variables = 3;
+
+console.log(variables);
