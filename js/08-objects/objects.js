fdb1ab9cf6e1927650db1f1f4c39dc47940495ff
cksumalinog <clarencekenzosumalinog@gmail.com>
2025-11-01 20:26:01 +0800

    Implemented objects in rps game

--- js/08-objects/objects.js
+++ js/08-objects/objects.js
@@ -1,3 +1,4 @@
+/*
 const product = {
   name: "socks",
   price: 1090,
@@ -15,3 +16,72 @@ console.log(product);
 
 delete product.date;
 console.log(product);
+*/
+
+const score = {
+  wins: 0,
+  losses: 0,
+  ties: 0,
+};
+
+function playGame(playerMove) {
+  const computerMove = pickComputerMove();
+
+  let result = "";
+  if (playerMove === "Scissor") {
+    if (computerMove === "Rock") {
+      result = "You Lose.";
+    } else if (computerMove === "Paper") {
+      result = "You Win!";
+    } else if (computerMove === "Scissors") {
+      result = "Tie.";
+    }
+  } else if (playerMove === "Paper") {
+    if (computerMove === "Rock") {
+      result = "You Win!";
+    } else if (computerMove === "Paper") {
+      result = "Tie.";
+    } else if (computerMove === "Scissors") {
+      result = "You Lose.";
+    }
+  } else if (playerMove === "Rock") {
+    if (computerMove === "Rock") {
+      result = "Tie.";
+    } else if (computerMove === "Paper") {
+      result = "You Lose.";
+    } else if (computerMove === "Scissors") {
+      result = "You Win!";
+    }
+  } else if (playerMove === "Reset") {
+    score.losses = 0;
+    score.ties = 0;
+    score.wins = 0;
+  }
+
+  if (result === "You Win!") {
+    score.wins += 1;
+  } else if (result === "You Lose.") {
+    score.losses += 1;
+  } else if (result === "Tie.") {
+    score.ties += 1;
+  }
+
+  alert(
+    `You picked ${playerMove}. Computer picked ${computerMove}. ${result}
+Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`
+  );
+}
+
+function pickComputerMove() {
+  const randomNum = Math.random();
+  let computerMove = "";
+  if (0 <= randomNum && randomNum < 1 / 3) {
+    computerMove = "Rock";
+  } else if (1 / 3 < randomNum && randomNum < 2 / 3) {
+    computerMove = "Paper";
+  } else if (2 / 3 < randomNum && randomNum < 1) {
+    computerMove = "Scissors";
+  }
+
+  return computerMove;
+}
