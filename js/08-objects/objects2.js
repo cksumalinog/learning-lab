a63d704c8a411f9451317f9ed4376e0b03931c70
cksumalinog <clarencekenzosumalinog@gmail.com>
2025-11-02 23:07:03 +0800

    Learned more about objects

--- js/08-objects/objects2.js
+++ js/08-objects/objects2.js
@@ -24,3 +24,44 @@ console.log(JSON.stringify(product2));
 
 const jsonString = JSON.stringify(product2);
 console.log(JSON.parse(jsonString));
+
+console.log("hello".length);
+console.log("hello".toUpperCase()); // auto-boxing
+
+const object1 = {
+  message: "hello",
+};
+const object2 = object1;
+
+object1.message = "Good Job";
+console.log(object1);
+console.log(object2);
+
+object2.message = "Bad Job";
+console.log(object2);
+
+const object3 = {
+  message: "Bad Job",
+};
+console.log(object3 === object1);
+
+const object4 = {
+  message: "Bad Job",
+  price: 799,
+};
+// const message = object4.message;
+const { message, price } = object4;
+console.log(message, price);
+
+const object5 = {
+  //message: message,
+  message,
+  //method: function function1() {
+  //  console.log("method");
+  //},
+  method() {
+    console.log("method");
+  },
+};
+console.log(object5);
+object5.method();
