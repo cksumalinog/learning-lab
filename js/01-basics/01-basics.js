78d4cfad7fdf10acbf22067a86df43a0e8aa2319
cksumalinog <clarencekenzosumalinog@gmail.com>
2025-11-09 01:23:23 +0800

    Rewatchin the course to better understand and to recall

---
+++ js/01-basics/01-basics.js
@@ -0,0 +1,24 @@
+// LESSON 1 EXERCISES - JAVASCRIPT BASICS
+
+// 1a
+//alert("Good morning!");
+
+// 1b
+//alert("Clarence Kenzo");
+
+// 1c
+console.log(10 + 5);
+
+// 1d
+console.log(20 - 5);
+
+// 1e
+console.log(2 + 2 - 5);
+
+// Challenge Exercises
+
+// 1h
+console.log(10 + 8 + 20);
+
+// 1i
+console.log(100 - 20 - 50 + 200);
---
+++ js/02-numbers&math/02-numbers&math.js
@@ -0,0 +1,31 @@
+/* Order of operations
+1. (parenthesis)
+2. * & /
+3. + & -
+It reads from left to right
+*/
+
+console.log(10.9 * 2 + 20.95);
+console.log(10.9 * 2 + 20.95 + 4.99); //with shipping fee
+console.log((10.9 * 2 + 20.95 + 4.99) * 0.1); // with tax
+
+/*
+Basketball = 20.95
+T-shirt = 7.99 (2)
+Tax = 10%
+*/
+console.log(20.95 + 7.99 * 2);
+console.log((20.95 + 7.99 * 2) * 0.1); // with tax
+
+console.log(20.95 + 7.99); // computers have a problem with floats/decimals numbers, it is inaacurate
+
+// best practice to avoid inaccuracies is to convert the money into cents
+
+console.log((2095 + 799) / 100); // now it only has 2 decimals, it rounds off
+
+// This is how to round off numbers to its nearest integer/who number
+console.log(Math.round(2.2));
+console.log(Math.round(2.8));
+
+console.log(((2095 + 799) * 0.1) / 100);
+console.log(Math.round((2095 + 799) * 0.1) / 100);
