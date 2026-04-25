a9eed5f5ec13b5f5264d440319c4355e2efc7811
cksumalinog <clarencekenzosumalinog@gmail.com>
2026-04-25 20:35:55 +0800

    exercises for 02

---
+++ js/new-learning-will-finish/02-numbers-and-math/02-numbers-and-math-exercise.js
@@ -0,0 +1,44 @@
+// 2.a
+console.log(10 + 3 * 8 + 5);
+
+// 2.b
+console.log((10 + 3 * 8 + 5) / 3);
+
+// 2.c
+console.log(18.5 + 7.5 * 2);
+
+// 2.d
+console.log((18.5 + 7.5 * 2) * 0.1);
+
+// 2.e
+console.log((18.5 + 7.5 * 2) * 0.2);
+
+// 2.f
+console.log((2095 + 799 + 1899) / 100);
+
+// 2.g
+console.log((2095 + 799 + 1899 + 499) / 100);
+
+// 2. h
+console.log(Math.round((2095 + 799 + 1899 + 499) * 0.1) / 100);
+
+// 2.i
+console.log(
+  (Math.round((2095 + 799 + 1899 + 499) * 0.1) + (2095 + 799 + 1899 + 499)) /
+    100,
+);
+
+// 2.j
+console.log(Math.floor(2.8));
+
+// 2.k
+console.log(Math.ceil(2.2));
+
+// 2.l
+console.log((25 * 9) / 5 + 32);
+
+// 2.m
+console.log(((86 - 32) * 5) / 9);
+
+// 2.n
+console.log((-5 * 9) / 5 + 32);
