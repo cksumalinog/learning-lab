8463a3892f2c9fb5c182ace86885f8aa0ab3388d
cksumalinog <clarencekenzosumalinog@gmail.com>
2026-04-27 00:05:46 +0800

    Relearned variables, will continue exercises later.

---
+++ js/new-learning-will-finish/05-variables/05-variables.js
@@ -0,0 +1,24 @@
+//let variable1 = 10; //let creates a new variable
+//console.log(variable1);
+
+// calculation = 2 + 2;
+//.log(calculation);
+//console.log(calculation + 2);
+//console.log(typeof calculation);
+
+//let result = calculation + 2;
+//console.log(result);
+
+//let message = "hello";
+//console.log(message);
+
+//variable1 = 67; // when reassigning variable do not type let
+//console.log(variable1);
+
+//variable1 = variable1 + 1;
+//console.log(variable1);
+
+const variable2 = 67;
+variable2 = 2;
+
+let cartQuantity = 0;
