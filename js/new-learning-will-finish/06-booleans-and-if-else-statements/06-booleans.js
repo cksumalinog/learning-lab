a72afa288f2793859338f45dd4820882ea14e80d
cksumalinog <clarencekenzosumalinog@gmail.com>
2026-04-28 22:55:44 +0800

    Booleans

--- js/new-learning-will-finish/06-booleans-and-if-else-statements/06-booleans.js
+++ js/new-learning-will-finish/06-booleans-and-if-else-statements/06-booleans.js
@@ -2,4 +2,19 @@ console.log(typeof true);
 console.log(3 > 5);
 
 console.log(5 == "5.00");
-console.log(5 === "5.00");
+console.log(5 === "5.00"); // prefered to check validity and avoid conversion bahaviour.
+
+if (false) {
+  console.log("Hello");
+} else {
+  console.log("Hi");
+}
+
+const age = 15;
+if (age >= 16) {
+  console.log("You can drive");
+} else if (age >= 14) {
+  console.log("Almost there!");
+} else {
+  console.log("You can not drive");
+}
