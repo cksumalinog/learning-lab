59a0e35bff48b5e0a67f8877ad47abf407ca6538
cksumalinog <clarencekenzosumalinog@gmail.com>
2026-04-28 23:44:44 +0800

    will continue tomo

---
+++ js/new-learning-will-finish/06-booleans-and-if-else-statements/06-booleans-rock-paper-scissor.js
@@ -0,0 +1,10 @@
+console.log("&& operator");
+console.log(true && true);
+console.log(true && false);
+console.log(0.2 >= 0 && 0.2 < 1 / 3);
+
+console.log("|| operator");
+console.log(true || false);
+
+console.log("! operator");
+console.log(!true);
