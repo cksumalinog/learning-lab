d961a2f6f5354dc25727bddcac26979684f487f4
cksumalinog <clarencekenzosumalinog@gmail.com>
2026-05-06 06:57:37 +0800

    finished the lesson for booleans, will continue the exercises later after school.

--- js/new-learning-will-finish/06-booleans-and-if-else-statements/06-booleans-rock-paper-scissor.js
+++ js/new-learning-will-finish/06-booleans-and-if-else-statements/06-booleans-rock-paper-scissor.js
@@ -8,3 +8,50 @@ console.log(true || false);
 
 console.log("! operator");
 console.log(!true);
+
+// Falsy Values - false, 0, '', NaN(Not a number), undefined, null
+//
+if (0) {
+  console.log("truthy");
+}
+
+const cartQuantity = 5;
+if (cartQuantity) {
+  console.log("cart has products");
+}
+
+console.log(!0);
+console.log("text" / 5);
+let variable1;
+const variable2 = undefined;
+console.log(variable1);
+console.log(variable2);
+
+// ternary operator
+const result = true ? "truthy" : "falsy";
+console.log("result");
+// ternary operator shortcut
+//if (true){
+//  'truthy'
+//}else {
+//  'falsy'
+//}
+
+//guard operator
+false && console.log("hello");
+const message = 5 && "hello";
+console.log(message);
+//guard operator shortcut
+// let message;
+// if(condition){
+//  message = 'hello'
+//}
+
+//default operator
+const currency = undefined || "USD";
+console.log(currency);
+//default operator shortcut
+//let currency;
+//if(!condition){
+//  currency = 'USD'
+//}
